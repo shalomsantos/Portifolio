d55af631f5102190e767105457987502df11ae0a
shalomsantos <shalomsantos1234@gmail.com>
2024-01-11 15:02:28 -0300

    melhor ainda

---
+++ js/script.js
@@ -0,0 +1,14 @@
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({ });
+
+const data = await octokit.paginate("GET /repos/{owner}/{repo}/issues", {
+  owner: "octocat",
+  repo: "Spoon-Knife",
+  per_page: 100,
+  headers: {
+    "X-GitHub-Api-Version": "2022-11-28",
+  },
+});
+
+console.log(data)

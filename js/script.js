267a79759ef269fa501a62f83739ffa935ca88d8
shalomsantos <shalomsantos1234@gmail.com>
2024-06-28 15:50:31 -0300

    melhoria de interface adicao de repositorios

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
-// import { Octokit } from "octokit";
+const apresentacao = document.querySelector('.apresentacao');
+const skills = document.querySelector('.skills');
+const repositorios = document.querySelector('.repositorios');
+const contatos = document.querySelector('.contatos');
 
-// const octokit = new Octokit({ });
-
-// const data = await octokit.paginate("GET /repos/{owner}/{repo}/issues", {
-//   owner: "octocat",
-//   repo: "Spoon-Knife",
-//   per_page: 50,
-//   headers: {
-//     "X-GitHub-Api-Version": "2022-11-28",
-//   },
-// });
-
-// console.log(data)
+function selectPage(name){
+    if(name == 'home'){
+        apresentacao.style.display = 'block';
+        skills.style.display = 'none';
+        repositorios.style.display = 'none';
+        contatos.style.display = 'none';
+    }else if(name == 'skills'){
+        apresentacao.style.display = 'none';
+        skills.style.display = 'block';
+        repositorios.style.display = 'none';
+        contatos.style.display = 'none';
+    }else if(name == 'repositorios'){
+        apresentacao.style.display = 'none';
+        skills.style.display = 'none';
+        repositorios.style.display = 'block';
+        contatos.style.display = 'none';
+    }else {
+        apresentacao.style.display = 'none';
+        skills.style.display = 'none';
+        repositorios.style.display = 'none';
+        contatos.style.display = 'block';
+    }
+    // skills.style.display = 'block';
+}
